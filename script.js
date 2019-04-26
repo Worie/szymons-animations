223b371a185c2c0ae5a0c8dac6b63700116307ac
Wojciech Połowniak <wojciech.polowniak@gmail.com>
2019-04-26 19:07:57 +0200

    created tasks for Szymon

---
+++ script.js
@@ -0,0 +1,43 @@
+let salary = prompt('Insert the salary youd wish to have');
+
+console.log(
+  'Wynik dzielenia pensji na 5 to',
+  salary / 5,
+  'Wynik reszta z dzielenia pensji na 5 to',
+  salary % 5
+);
+
+let nazwaZmiennej;        // Tworzy nową zmienna
+let nazwaZmiennej2 = 5;   // Tworzy nową zmienną i nadaje jej wartość 5
+
+console.log('text');               // wyswietli w kosnolki przegladarki 'text'
+console.log(nazwaZmiennej2);       // wyswietli w kosnolki przegladarki 5 (wartosc zmiennej)
+console.log(5 + 5);                // wyswietli w konsoli przegladrki wynik operacji matematycznej (10)
+
+// podstawowe operatory w JavaScript:
+// + dodawanie
+// - odejmowanie
+// * mnozenie
+// / dzielenie
+// % reszta z dzielenia
+// > większe niz 
+// < mniejsze niz 
+// = operator przypisania (tylko przy zmiennych!)
+// == operator porówniania 
+
+// Zadanie 1:
+// 1. dwa razy poprosić uzytkownika o dane za pomoca prompt
+// 2. do dwóch zmiennych przypisac wartosci ktore uzytkownik wpisal
+// 3. dodac je do siebie 
+// 3.1 odjąć je do siebie 
+// 3.2 podzielic
+// 3.3 pomnozyc
+// 3.4 porownac
+// 3.5 sprawdzic reszte z dzielenia
+// 4. wyswietlic w konsoli (console.log)
+
+// Zadanie 2: 
+// 1. Pomyśleć, do czego mozna jeszcze wykorzystać to co juz wiesz w kontekscie JS
+
+// Zadanie 3: 
+// Custom-styled checkboxes: https://i2.wp.com/www.cssscript.com/wp-content/uploads/2017/10/Custom-Checkbox-And-Radio-Inputs-In-Pure-CSS.png?fit=503%2C373&ssl=1
